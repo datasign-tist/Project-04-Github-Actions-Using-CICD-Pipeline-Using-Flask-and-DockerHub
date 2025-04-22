8d873841b7b31adf5809ec4b107a348f07c62720
datasign-tist <abhishek.kumar.sharma2697@gmail.com>
2025-04-22 23:42:08 +0530

    Updated Flask App

--- app.py
+++ app.py
@@ -1,18 +1,14 @@
 from flask import Flask, render_template
 
-def create_app():
-    app = Flask(__name__)
+app = Flask(__name__)
 
-    @app.route("/")
-    def home():
-        return render_template("home.html")
+@app.route("/")
+def home():
+    return render_template("home.html")
 
-    @app.route("/about")
-    def about():
-        return render_template("about.html")
-
-    return app
+@app.route("/about")
+def about():
+    return render_template("about.html")
 
 if __name__ == "__main__":
-    app = create_app()
     app.run(host="0.0.0.0", port=5000, debug=True)
